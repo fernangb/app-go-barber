f8e0ca59e9361769f5000e2c7223df21eb0e202e
Gabriel Fernandes <gfsouza@poli.ufrj.br>
2020-07-25 20:31:33 -0300

    First commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <View>
+      <Text>Oi</Text>
+    </View>
+  );
+};
+
+export default App;
