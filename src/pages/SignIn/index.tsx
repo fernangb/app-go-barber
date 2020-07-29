6989462e7af8b22a392d969c904aa3136957a2c1
Gabriel Fernandes <gfsouza@poli.ufrj.br>
2020-07-28 22:03:47 -0300

    Input e Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import {Image} from 'react-native';
 import {Container, Title} from './styles';
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça o seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Oi');
+        }}>
+        Entrar
+      </Button>
     </Container>
   );
 };
